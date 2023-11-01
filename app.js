dc65aa739ad6c31e755402aa9bf712bf83e6d0ae
Richard Marshall <rich.qazwsx@gmail.com>
2023-11-01 13:52:16 -0700

    hit detection

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const canvas = document.getElementById('spriteContainer');
 const ctx = canvas.getContext('2d');
+const startButton = document.getElementById('startButton')
+const resetButton = document.getElementById('resetButton')
+
+startButton.addEventListener("click", () => startGame());
+resetButton.addEventListener("click", () => resetGame());
+
+let intervalID;
+let score = 0;
 
 // Load sprite sheets for different animations
 const spriteSheets = {
@@ -25,15 +33,14 @@ const totalFrames = {
 const frameDelay = 150; // Adjust the delay to control animation speed (in milliseconds)
 
 const ghostDelay = 5500; // Delay for adding new ghosts in milliseconds
-const ghosts = []; // Empty array to store ghosts
+let ghosts = []; // Empty array to store ghosts
 
 // Function to add a new ghost to the array
 function addNewGhost() {
-  ghosts.push({ x: Math.random() * (canvas.width - frameWidth), y: 0, currentFrame: 0, animation: 'idle' });
+  ghosts.push({ x: Math.random() * (canvas.width - frameWidth), y: 0, currentFrame: 0, animation: 'idle', state: 'active', });
 }
 
-// Initialize by adding the first ghost
-addNewGhost();
+
 
 // Black box properties
 const boxWidth = 25;
@@ -43,11 +50,26 @@ const boxY = canvas.height - boxHeight; // Y position at the bottom of the canva
 
 // Fired cube properties
 const cubeSize = 10;
-const firedCubes = []; // Array to store fired cubes
+let firedCubes = []; // Array to store fired cubes
 let canFire = true;
 
 // Store the timestamp of the previous frame
 let lastTimestamp;
+
+function startGame() {
+  // Initialize by adding the first ghost
+  addNewGhost();
+
+  intervalID = setInterval(() => {
+    addNewGhost();
+  }, ghostDelay);
+}
+
+function resetGame() {
+  clearInterval(intervalID)
+  ghosts = [];
+  firedCubes = [];
+}
 
 // Function to draw a single frame for a sprite
 function drawSingleGhost(ghost) {
@@ -133,9 +155,39 @@ function animate(timestamp) {
   if (elapsed >= frameDelay) {
     clearCanvas();
 
-    // Update and draw sprites
-    for (const ghost of ghosts) {
-      drawSingleGhost(ghost);
+    for (let i = 0; i < ghosts.length; i++) {
+      const ghost = ghosts[i];
+      if (ghost.state === 'active') {
+        drawSingleGhost(ghost);
+
+        // Iterate through fired cubes and check for collisions
+        for (let j = 0; j < firedCubes.length; j++) {
+          const cube = firedCubes[j];
+
+          if (
+            cube.x < ghost.x + frameWidth &&
+            cube.x + cubeSize > ghost.x &&
+            cube.y < ghost.y + frameHeight &&
+            cube.y + cubeSize > ghost.y
+          ) {
+            // Collision detected
+            ghosts[i].state = 'hit'
+            firedCubes.splice(j, 1);
+            j--
+          }
+        }
+      } else if (ghost.state === 'hit') {
+
+        console.log("ghost was hit");
+
+        ghost.state = 'dead';
+
+        console.log('ghost dead');
+        ghosts.splice(i, 1); // Remove the ghost from the array
+         // Remove the ghost from the array
+        i--;
+        
+      }
     }
 
     // Draw the black box
@@ -163,6 +215,4 @@ function animate(timestamp) {
 // Start the global animation loop
 animate();
 
-setInterval(() => {
-  addNewGhost();
-}, ghostDelay);
+
